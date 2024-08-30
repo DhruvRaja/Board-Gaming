51250535ce184c8a59fb509b5a2b2897cd2d1a96
DhruvRaja <21amtics463@gmail.com>
2024-08-30 12:58:13 +0530

    initial commit

---
+++ assets/js/board.js
@@ -0,0 +1,15 @@
+if (window['AOS']) {
+    window['AOS'].init({
+        duration:1200,
+    });
+}
+
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+    
